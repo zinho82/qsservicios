a6e15d80f716694a2493c8d5dbd753130a7ac2f9
Analista 01 <Analista 01@QS-10>
2017-04-25 11:20:58 -0300

    Correccion lista encuestas No mostrar encuesta finalizada ni contestada

--- modules/encuestas/view/hcenter_index.php
+++ modules/encuestas/view/hcenter_index.php
@@ -6,7 +6,7 @@ require_once '../../config/superior.php';
  * and open the template in the editor.
  */
 $conn = new config();
-$_SESSION['campana']['bd']='qsschile_qs_encuestas';
+$_SESSION['campana']['bd'] = 'qsschile_qs_encuestas';
 ?>
 <div class="panel panel-primary">
     <div class="panel-heading">Encuestas Homcenter Empresas</div>
@@ -22,15 +22,15 @@ $_SESSION['campana']['bd']='qsschile_qs_encuestas';
         </thead>
         <tbody >
             <?php
-              $sql = "select * from qsschile_qs_encuestas.qs_encuestascli_sodimac_emp  sm where sm.id_acceso=".$_SESSION['usuario']['id']." and sm.estado!=30  and id_encuesta!=0 and datediff(date(now()),date(sm.fec_termino))>0  and sm.num_post<6";
+            $sql = "select * from qsschile_qs_encuestas.qs_encuestascli_sodimac_emp  sm where sm.id_acceso=" . $_SESSION['usuario']['id'] . " and (sm.estado!=30 and  sm.estado!=7) and id_encuesta!=0 and datediff(date(now()),date(sm.fec_termino))>0  and sm.num_post<6";
             $res = mysql_query($sql, $conn->conectar()) or die(mysql_error());
             while ($enc = mysql_fetch_assoc($res)) {
                 echo "<tr><td>" . $enc['cod_carga'] . "</td>"
                 . "<td>" . $enc['id_encuesta'] . "</td>"
-                        . "<td>" . $enc['rut'] . "</td>"
+                . "<td>" . $enc['rut'] . "</td>"
                 . "<td>" . $enc['Cliente'] . "</td>"
-                . "<td>" .$conn->BuscaDatos($_SESSION['campana']['bd'], 'arbol', $enc['estado'] , 'idarbol', 'texto') . "</td>"
-                        . "<td>" . $enc['fec_termino'] . "</td>"
+                . "<td>" . $conn->BuscaDatos($_SESSION['campana']['bd'], 'arbol', $enc['estado'], 'idarbol', 'texto') . "</td>"
+                . "<td>" . $enc['fec_termino'] . "</td>"
                 . "<td><a href='" . __BASE_URL__ . "modules/encuestas/view/Enc_hc_emp.php?id_encuesta=" . $enc['id_encuesta'] . "&id_formato=12&estado=2' ><i class='fa fa-search'></i></a></td>"
                 . "</tr>";
             }
@@ -39,5 +39,5 @@ $_SESSION['campana']['bd']='qsschile_qs_encuestas';
     </table>
 </div>
 
-<script src="<?php echo __BASE_URL__.__MODULO_Encuestas__ ?>js/hcenter_js.js"></script>
+<script src="<?php echo __BASE_URL__ . __MODULO_Encuestas__ ?>js/hcenter_js.js"></script>
 <?php require_once '../../config/superior.php'; ?>
